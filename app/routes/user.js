9d92a625bbbc034d817fd75359cd3d69bd4bc4ed
Unknown <elienhard@csu.fullerton.edu>
2017-12-02 15:09:43 -0800

    Query for most recent grocery lists

--- app/routes/user.js
+++ app/routes/user.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model: function() {
-        return this.setProperties.findAll('id');
-    }
-});
+        var userid = this.session.get('session.currentUser.uid');
+        return this.store.query('user', {
+          userId: userid,
+        });
+}});
