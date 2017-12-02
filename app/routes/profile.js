cbc04b1525998a2e128462fbaa205caf632cbea7
Justin Shelley <justins430@gmail.com>
2017-12-01 20:49:16 -0800

    Added a button to add list items into store so they will show up in Create List

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -1,4 +1,81 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+  return [{
+    "userName": "Me",
+    "id": '1',
+    "listItems": [{
+        "itemAmt": 1,
+        "itemName": "Milk",
+        "itemDesc": "Comes from a cow"
+      },
+      {
+        "itemAmt": 2,
+        "itemName": "Eggs",
+        "itemDesc": "Comes from a chicken"
+      }
+    ]
+  },
+  {
+    "userName": "Me",
+    "id": '1',
+    "listItems": [{
+        "itemAmt": 3,
+        "itemName": "Milk",
+        "itemDesc": "Comes from a cow"
+      },
+      {
+        "itemAmt": 4,
+        "itemName": "Eggs",
+        "itemDesc": "Comes from a chicken"
+      }
+    ]
+  },
+  {
+    "userName": "Me",
+    "id": '1',
+    "listItems": [{
+        "itemAmt": 5,
+        "itemName": "Milk",
+        "itemDesc": "Comes from a cow"
+      },
+      {
+        "itemAmt": 6,
+        "itemName": "Eggs",
+        "itemDesc": "Comes from a chicken"
+      }
+    ]
+  },
+  {
+    "userName": "Me",
+    "id": '1',
+    "listItems": [{
+        "itemAmt": 7,
+        "itemName": "Milk",
+        "itemDesc": "Comes from a cow"
+      },
+      {
+        "itemAmt": 8,
+        "itemName": "Eggs",
+        "itemDesc": "Comes from a chicken"
+      }
+    ]
+  },
+  {
+    "userName": "Me",
+    "id": '1',
+    "listItems": [{
+        "itemAmt": 9,
+        "itemName": "Milk",
+        "itemDesc": "Comes from a cow"
+      },
+      {
+        "itemAmt": 10,
+        "itemName": "Eggs",
+        "itemDesc": "Comes from a chicken"
+      }
+    ]
+  }];
+}
 });
