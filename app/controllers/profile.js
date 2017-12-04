47e2e68fc423b463da6a2e700e7c132092f7aa4d
Justin Shelley <justins430@gmail.com>
2017-12-03 16:23:56 -0800

    Cleaned up profile controller

--- app/controllers/profile.js
+++ app/controllers/profile.js
@@ -4,50 +4,8 @@ import Ember from 'ember';
 var indexNum = 0;
 
 export default Controller.extend({
-  lists: null,
-
-  listItems: Ember.computed(function() {
-    return this.store.findAll('list-item');
-  }),
-
   uid: Ember.computed(function() {
     return this.get('session.currentUser.uid');
-  }),
-
-  shoppingList: Ember.computed(function() {
-
-    /*
-    let templateLists = [];
-    let lists = this.get('model');
-    let that = this;
-    lists.forEach((list) => {
-        let id = list.get('listId');
-        let innerList = {};
-        innerList[id] = {};
-        innerList[id]['timeStamp'] = list.get('timeStamp');
-        innerList[id]['items'] = [];
-        //templateLists[id]['items'] =
-        that.store.query('list-item', {
-          orderBy: 'listId',
-          equalTo: id
-        }).then((items) => {
-          items.forEach((items) => {
-            let amt = items.get('itemAmt');
-            let itemName = items.get('itemName');
-            let listId = items.get('listId');
-            let item = {
-              'itemAmt': amt,
-              'itemName': itemName,
-              'listId': listId
-            };
-            innerList[id]['items'].push(item);
-          });
-        });
-        templateLists.push(innerList);
-      });
-        console.log(templateLists);
-        return templateLists;
-        */
   }),
 
   actions: {
